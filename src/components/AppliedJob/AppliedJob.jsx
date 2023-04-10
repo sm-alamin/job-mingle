ac00eec9e8339311c7b48d057d13557307af7892
sm-alamin <sma.fkml@gmail.com>
2023-04-11 00:10:26 +0600

    filter system is added on applied job component

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -1,20 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { getJobInformation } from '../../utilities/fakedb';
-import { useLoaderData } from 'react-router-dom';
-import AppliedPost from '../AppliedPost/AppliedPost';
+import React, { useEffect, useState } from "react";
+import { getJobInformation } from "../../utilities/fakedb";
+import { useLoaderData } from "react-router-dom";
+import AppliedPost from "../AppliedPost/AppliedPost";
 
 const AppliedJob = () => {
-    const {initialJobCartInfo} = useLoaderData();
+  const { initialJobCartInfo } = useLoaderData();
+  const [filteredJobs, setFilteredJobs] = useState(initialJobCartInfo);
 
-    return (
-        <div>
-            <div className='grid grid-cols-1 gap-8' >
-            {
-                initialJobCartInfo.map(post => <AppliedPost key={post.id} post ={post} />)
-            }
-            </div>
-        </div>
-    );
+  const handleFilter = (filterOption) => {
+    if (filterOption === "remote") {
+      const remoteJobs = initialJobCartInfo.filter((job) => job.remote_or_onsite === "Remote");
+      setFilteredJobs(remoteJobs);
+    } else if (filterOption === "onsite") {
+      const onsiteJobs = initialJobCartInfo.filter((job) => job.remote_or_onsite === "Onsite");
+      setFilteredJobs(onsiteJobs);
+    } else {
+      setFilteredJobs(initialJobCartInfo);
+    }
+  };
+  
+
+  return (
+    <div>
+      <div className="form-control w-32 max-w-xs py-5 ml-auto">
+        <select className="select select-bordered" defaultValue="all" onChange={(e) => handleFilter(e.target.value)}>
+          <option disabled value="all">
+            Filter By
+          </option>
+          <option value="remote">Remote</option>
+          <option value="onsite">On-site</option>
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-8">
+        {filteredJobs.map((post) => (
+          <AppliedPost key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AppliedJob;
